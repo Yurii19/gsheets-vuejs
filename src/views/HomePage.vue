7bc348c6340f821a9b561f34ea112b725c044fed
Yurii19 <yurikolechkin@gmail.com>
2023-03-09 09:26:04 +0200

    style: button login and logout replaced by bootstrap

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,8 @@
 <template lang="html">
   <section class="home">
     <h1>home Component</h1>
+    <button type="button" class="btn btn-secondary btn-sm">Small button</button>
+    <button type="button" class="btn btn-secondary ">Small button</button>
   </section>
 </template>
  
