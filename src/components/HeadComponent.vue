0ff615fb1b5b704683670de117b6defaa4f0ebed
Yurii19 <yurikolechkin@gmail.com>
2023-03-10 17:13:11 +0200

    chore: got first successful response

--- src/components/HeadComponent.vue
+++ src/components/HeadComponent.vue
@@ -18,57 +18,112 @@
         Log out
       </button>
 
-      <GoogleLogin :callback="loginCallback" />
+      <GoogleLogin :callback="onLogin" />
     </div>
   </div>
 </template>
 
 <script setup>
-//import { CLIENT_ID } from '@/variables/constants';
-import { CLIENT_ID, SCOPES } from '@/variables/constants';
+import {
+  API_KEY,
+  CLIENT_ID,
+  DISCOVERY_DOC,
+  SCOPES,
+  SHEET,
+} from '@/variables/constants';
 import { onMounted } from 'vue';
-import { googleLogout } from 'vue3-google-login';
-import { decodeCredential } from 'vue3-google-login';
+//import { googleLogout } from 'vue3-google-login';
+//import { decodeCredential } from 'vue3-google-login';
 import { googleSdkLoaded } from 'vue3-google-login';
 
-let gCredential = {};
-let uCredentials = {};
-//let googleClient = {};
+// let gCredential = {};
+// let uCredentials = {};
+let googleClient = {};
 
 onMounted(function () {
-  console.log('onMounted');
+  window.gapi.load('client', {
+    initializeGapiClient,
+  });
+  // setTimeout(() => {
+  //    console.log('setTimeout->window.gapi: ', window.gapi);
+  //  // initializeGapiClient()
+  // }, 2000);
+
+  console.log('onMounted:  window.gapi.client: ', window.gapi);
 });
 
-const loginCallback = (resp) => {
-  setCredential(resp);
-  googleSdkLoaded((google) => {
-    console.log('gogle: ', google);
-   const gapi = google.accounts.oauth2
+const initializeGapiClient = async function () {
+  console.log('initializeGapiClient called.', window.gapi.client);
+  await window.gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: [DISCOVERY_DOC],
+      scope: SCOPES,
+    })
+    .then((d) => console.log('initializeGapiClient: ', d));
+  // gapiInited = true;
+  // maybeEnableButtons();
+};
+
+const onLogin = (gUser) => {
+  const authResponse = gUser;
+  console.log('onSuccess authResponse: -> ', authResponse);
+  googleSdkLoaded((gSdk) => {
+    console.log('googleSdkLoaded: ', gSdk);
+    gSdk.accounts.oauth2
       .initCodeClient({
         client_id: CLIENT_ID,
         scope: SCOPES,
         callback: (response) => {
-          //googleClient = response;
-          console.log('Handle the response', response);
+          googleClient = response;
+          console.log('Handle the response', googleClient);
         },
       })
       .requestCode();
-      console.log('gapi: ',gapi)
   });
 };
 
 const gLogOut = () => {
-  //const res = getCredentials()
-  console.log('this: ' );
-  console.log('gCredential', gCredential);
-  console.log('decodedCredentials', uCredentials);
-  googleLogout();
+  //console.log('googleClient => ', googleClient )
+   window.gapi.client.request({
+    path: SHEET,
+    method: 'GET',
+    params: { key: API_KEY}
+  }).then((r)=> console.log('gLogOut >>> ',r));
+  // console.log('gLogOut:  window.gapi: ', window.gapi);
+  // console.log('gLogOut:  window.gapi.client: ', window.gapi.client);
+  //console.log('gLogOut:  window.gapi.client.sheets: ', gClient);
+
+  // googleLogout();
 };
 
-const setCredential = function (response) {
-  gCredential = response;
-  uCredentials = decodeCredential(response.credential);
-};
+// const loginCallback = (gUser) => {
+//   //setCredential(resp);
+//   const authResponse = gUser;
+//   console.log('authResponse: ', authResponse)
+
+//   googleSdkLoaded((google) => {
+//     //console.log('gogle: ', google);
+//     google.accounts.oauth2
+//       .initCodeClient({
+//         client_id: CLIENT_ID,
+//         scope: SCOPES,
+//         callback: (response) => {
+//           //googleClient = response;
+//           googleClient = response;
+//           console.log(googleClient)
+//           console.log('Handle the response', response);
+//         },
+//       })
+//       .requestCode();
+//   });
+// };
+
+// const setCredential = function (response) {
+//   gCredential = response;
+//   uCredentials = decodeCredential(response.credential);
+// };
 // const loginCallback1 = (response) => {
 //   gCredential = response;
 //   //decodedCredentials = decodeCredential(response.credential);
--- src/variables/constants.js
+++ src/variables/constants.js
@@ -2,9 +2,15 @@ export const CLIENT_ID =
   '776632151768-dt2qeel2mfdadh6caugakqu0kdqfcn65.apps.googleusercontent.com';
 
 export const API_KEY = 'AIzaSyDoSVuRZno6d22QwQjOuQhSY_woW6vivDM';
+
 export const DISCOVERY_DOC =
   'https://sheets.googleapis.com/$discovery/rest?version=v4';
-export const SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+export const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 export const SCOPES =
   'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/drive';
 export const REDIRECT_URI = 'http://localhost:8080';
+
+export const SHEET_2 =
+  'https://docs.google.com/spreadsheets/d/1b9HuDKyFU6mxDqzjmY9l0Q249br5J1DjK0b3Z9TokGw/edit#gid=1426943389';
+export const SHEET =
+  'https://sheets.googleapis.com/v4/spreadsheets/1b9HuDKyFU6mxDqzjmY9l0Q249br5J1DjK0b3Z9TokGw';
